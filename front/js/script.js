437bc4880eb380fc733d7fdfac43ab844c536122
Otsukionata <e-ramassamy@laposte.net>
2022-09-28 16:06:09 +0200

    Affichage des produits en page d'accueil

---
+++ front/js/script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+async function getProducts() {
+  const Response = await fetch("http://localhost:3000/api/products");
+  const Products = await Response.json();
+  // console.log(Products);
+  createProduct(Products);
+}
+getProducts();
+
+function createProduct(product) {
+  for (let i = 0; i < product.length; i++) {
+    // Balise récupérant les cartes dans le DOM
+    const SectionCards = document.querySelector("#items");
+
+    const Cards = document.createElement("article");
+    // Cards.dataset.id = product[i]._id;
+    // Cards.src = product[i].
+
+    const ImgKanap = document.createElement("img");
+    ImgKanap.src = product[i].imageUrl;
+    ImgKanap.alt = product[i].altTxt;
+    Cards.appendChild(ImgKanap);
+
+    const NameKanap = document.createElement("h3");
+    NameKanap.innerText = product[i].name;
+    NameKanap.classList.add("productName");
+    Cards.appendChild(NameKanap);
+
+    const DescriptionKanap = document.createElement("p");
+    DescriptionKanap.innerText = product[i].description;
+    DescriptionKanap.classList.add("productDescription");
+    Cards.appendChild(DescriptionKanap);
+
+    SectionCards.appendChild(Cards);
+  }
+}

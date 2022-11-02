377bfa4d4aa25b5638588426b057e7f6d5e18995
Otsukionata <e-ramassamy@laposte.net>
2022-11-02 13:08:59 +0100

    début de travail sur le formulaire

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,15 +1,12 @@
 "use strict";
 console.log("Mon panier");
 
-// Cacher le formulaire
-document.querySelector(".cart__order").style.display = "none";
-
-// Affichage du nom de la page dans la balise Title pour l'onglet
+// =====================  Affichage du nom de la page dans la balise Title pour l'onglet
 const Title = document.querySelector("title");
 Title.innerText = "Panier";
 
 function getCart() {
-  //  1. Récupération et affichage des données du panier enregistré dans le LS
+  //  =====================  Récupération et affichage des données du panier enregistré dans le LS
   let cart = localStorage.getItem("cart");
 
   // Ce qu'il se passe si le panier est rempli ou vide
@@ -20,90 +17,13 @@ function getCart() {
     return [];
   } else {
     console.log("Il y a des articles dans le panier !");
-    // console.log(cart);
 
     return JSON.parse(cart);
   }
 }
 let cart = getCart();
 
-//  Fonction de sauvegarde des modifications du panier
-function saveCart(data) {
-  localStorage.setItem("cart", JSON.stringify(data));
-}
-
-// L'affichage des produits
-cart.forEach((data) => productDisplay(data));
-
-function productDisplay(data) {
-  const DisplayArticle = displayArticle(data);
-  container(DisplayArticle);
-
-  const DisplayImage = displayImage(data);
-  const DisplayDescription = displayDescription(data);
-  const DisplaySettings = settings(data);
-
-  DisplayArticle.appendChild(DisplayImage);
-  DisplayArticle.appendChild(DisplayDescription);
-  DisplayArticle.appendChild(DisplaySettings);
-}
-
-function container(DisplayArticle) {
-  document.querySelector("#cart__items").appendChild(DisplayArticle);
-}
-
-//  --------- Création de la balise article
-function displayArticle(product) {
-  const Article = document.createElement("article");
-  Article.classList.add("cart__item");
-  Article.dataset.id = product.id;
-  Article.dataset.color = product.color;
-
-  return Article;
-}
-
-// Affichage de l'image avec sa description
-function displayImage(product) {
-  const ImageDiv = document.createElement("div");
-  ImageDiv.classList.add("cart__item__img");
-
-  const Image = document.createElement("img");
-  Image.src = product.image;
-  Image.alt = product.altImg;
-
-  ImageDiv.appendChild(Image);
-  return ImageDiv;
-}
-
-// Caractéristiques de chaque produit choisi : Nom, Couleur, Prix
-function displayDescription(product) {
-  const Container = document.createElement("div");
-  Container.classList.add("cart__item__content");
-
-  const ItemDescription = document.createElement("div");
-  ItemDescription.classList.add("cart__item__content__description");
-
-  // Nom du produit
-  const ProductName = document.createElement("h2");
-  ProductName.innerText = product.name;
-
-  // Couleur choisie
-  const ProductColor = document.createElement("p");
-  ProductColor.innerText = product.color;
-
-  // Stockage en variables des id des articles et des conteneurs à créer dans le HTML
-  const Price = priceSettings(product);
-
-  // Rattachement de ces 3 élements à la div les contenant
-  ItemDescription.appendChild(ProductName);
-  ItemDescription.appendChild(ProductColor);
-  ItemDescription.appendChild(Price);
-
-  // Ajout de la div "ItemDescription" à la fiche du produit
-  Container.appendChild(ItemDescription);
-  return Container;
-}
-
+// =====================  Récupération et affichage des prix de chaque produit via l'API
 function priceSettings(data) {
   const ID = data.id;
   const Price = document.createElement("p");
@@ -128,6 +48,92 @@ function priceSettings(data) {
 
   return Price;
 }
+
+
+//  =====================  Fonction de sauvegarde des modifications du panier
+function saveCart(data) {
+  localStorage.setItem("cart", JSON.stringify(data));
+}
+
+// let cartCopy = [];
+
+// console.log(cart);
+// // =====================  L'affichage des produits
+// cart.forEach((data) => {
+//   data.price = priceSettings(data);
+//   cartCopy.push(data);
+// });
+
+// console.log(cartCopy);
+
+function productDisplay(data) {
+  const DisplayArticle = displayArticle(data);
+  container(DisplayArticle);
+
+  const DisplayImage = displayImage(data);
+  const DisplayDescription = displayDescription(data);
+  const DisplaySettings = settings(data);
+
+  DisplayArticle.appendChild(DisplayImage);
+  DisplayArticle.appendChild(DisplayDescription);
+  DisplayArticle.appendChild(DisplaySettings);
+}
+
+function container(DisplayArticle) {
+  document.querySelector("#cart__items").appendChild(DisplayArticle);
+}
+
+//  =====================  Création de la balise article
+function displayArticle(product) {
+  const Article = document.createElement("article");
+  Article.classList.add("cart__item");
+  Article.dataset.id = product.id;
+  Article.dataset.color = product.color;
+
+  return Article;
+}
+
+// =====================  Affichage de l'image avec sa description
+function displayImage(product) {
+  const ImageDiv = document.createElement("div");
+  ImageDiv.classList.add("cart__item__img");
+
+  const Image = document.createElement("img");
+  Image.src = product.image;
+  Image.alt = product.altImg;
+
+  ImageDiv.appendChild(Image);
+  return ImageDiv;
+}
+
+// =====================  Caractéristiques de chaque produit choisi : Nom, Couleur, Prix
+function displayDescription(product) {
+  const Container = document.createElement("div");
+  Container.classList.add("cart__item__content");
+
+  const ItemDescription = document.createElement("div");
+  ItemDescription.classList.add("cart__item__content__description");
+
+  // Nom du produit
+  const ProductName = document.createElement("h2");
+  ProductName.innerText = product.name;
+
+  // Couleur choisie
+  const ProductColor = document.createElement("p");
+  ProductColor.innerText = product.color;
+
+  const Price = document.createElement("p");
+
+  // Rattachement de ces 3 élements à la div les contenant
+  ItemDescription.appendChild(ProductName);
+  ItemDescription.appendChild(ProductColor);
+  ItemDescription.appendChild(Price);
+
+  // Ajout de la div "ItemDescription" à la fiche du produit
+  Container.appendChild(ItemDescription);
+  return Container;
+}
+
 
 // Affichage des quantités et de leurs boutons associés
 function quantity(product) {
@@ -154,7 +160,7 @@ function quantityInput(product) {
   QuantityChange.setAttribute("value", product.quantity);
   QuantityChange.setAttribute("aria-label", "Nombre d'articles");
 
-  // Modification des quantités
+  // =====================  Modification des quantités
   QuantityChange.addEventListener("change", function () {
     product.quantity = Number(this.value);
 
@@ -166,7 +172,7 @@ function quantityInput(product) {
   return QuantityChange;
 }
 
-// La suppression d'articles
+// =====================  La suppression d'articles
 function deleteProduct() {
   const DeleteBtn = document.createElement("div");
   DeleteBtn.classList.add("cart__item__content__settings__delete");
@@ -197,7 +203,7 @@ function deleteProduct() {
   return DeleteBtn;
 }
 
-// Rattachement des changements du panier (suppression, quantité) au HTML
+// =====================  Rattachement des changements du panier (suppression, quantité) au HTML
 function settings(data) {
   const Settings = document.createElement("div");
   const Quantities = quantity(data);
@@ -209,7 +215,7 @@ function settings(data) {
   return Settings;
 }
 
-//  Afficher la quantité totale d'articles du panier
+//  =====================  Afficher la quantité totale d'articles du panier
 function numberOfItems() {
   const TotalQuantity = document.querySelector("#totalQuantity");
   let number = [];
@@ -224,11 +230,180 @@ numberOfItems();
 
 // Fin de la fonction d'affichage
 
-// Gestion du formulaire
+// =====================  Gestion du formulaire
+const SubmitBtn = document.querySelector(".cart__order__form__submit");
 
 /**
- *   const validEmail = function (inputEmail) {
-  let emailRegExp = new RegExp(
-      /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-    )};
+ *   if (localStorage.getItem("client")) {
+    cart = JSON.parse(localStorage.getItem("client"));
+  }
  */
+
+let forename = document.querySelector("#firstName");
+let surname = document.querySelector("#lastName");
+let address = document.querySelector("#address");
+let city = document.querySelector("#city");
+let mail = document.querySelector("#email");
+
+let forenameErrMsg = document.querySelector("#firstNameErrorMsg");
+let surnameErrMsg = document.querySelector("#lastNameErrorMsg");
+let addressErrMsg = document.querySelector("#addressErrorMsg");
+let cityErrMsg = document.querySelector("#cityErrorMsg");
+let mailErrMsg = document.querySelector("#emailErrorMsg");
+
+let forenameInput, surnameInput, addressInput, cityInput, mailInput;
+
+forename.addEventListener("input", function (event) {
+  console.log(forename);
+  forenameInput;
+  if (event.target.value.length <= 1) {
+    forenameInput = null;
+    forenameErrMsg.innerText = "Veuillez entrer votre prénom";
+    console.log("Champs vide", forenameInput);
+  } else if (event.target.value.length < 2 || event.target.value.length > 25) {
+    forenameErrMsg.innerText = "Le prénom doit avoir entre 2 et 25 caractères";
+    forenameInput = null;
+  }
+  if (event.target.value.match(/^[a-z A-Z À-ÿ-]{2,25}$/u)) {
+    forenameErrMsg.innerText = "";
+    forenameInput = event.target.value;
+    console.log("Champs non vide", forenameInput);
+  } else {
+    console.log("Veuillez entrer votre prénom");
+    forenameErrMsg.innerText = "Veuillez entrer un prénom valide";
+  }
+});
+
+surname.addEventListener("input", function (event) {
+  console.log(surname);
+  surnameInput;
+  if (event.target.value.length <= 1) {
+    surnameInput = null;
+    surnameErrMsg.innerText = "Veuillez entrer votre nom";
+    console.log("Champs vide", surnameInput);
+  } else if (event.target.value.length < 2 || event.target.value.length > 25) {
+    surnameErrMsg.innerText = "Le nom doit avoir entre 2 et 25 caractères";
+    surnameInput = null;
+  }
+  if (event.target.value.match(/^[a-z A-Z  À-ÿ -]{2,25}$/)) {
+    surnameErrMsg.innerText = "";
+    surnameInput = event.target.value;
+    console.log("Champs non vide", surnameInput);
+  } else {
+    surnameErrMsg.innerText = "Veuillez entrer un nom valide";
+  }
+});
+
+address.addEventListener("input", function (event) {
+  console.log(address);
+  addressInput;
+  if (event.target.value.length == 0) {
+    addressInput = null;
+    addressErrMsg.innerText = "Veuillez entrer votre ville";
+    console.log("Champs vide", addressInput);
+  } else if (event.target.value.length < 1 || event.target.value.length > 42) {
+    addressErrMsg.innerText = "La ville doit avoir entre 1 et 45 caractères";
+    addressInput = null;
+  }
+  if (event.target.value.match(/^[0-9][a-x] [a-z A-Z À-ÿ - 0-9]$/)) {
+    addressErrMsg.innerText = "";
+    addressInput = event.target.value;
+    console.log("Champs non vide", addressInput);
+  }
+});
+
+city.addEventListener("input", function (event) {
+  console.log(city);
+  cityInput;
+  if (event.target.value.length == 0) {
+    cityInput = null;
+    cityErrMsg.innerText = "Veuillez entrer votre ville";
+    console.log("Champs vide", cityInput);
+  } else if (event.target.value.length < 1 || event.target.value.length > 42) {
+    cityErrMsg.innerText = "La ville doit avoir entre 1 et 45 caractères";
+    cityInput = null;
+  }
+  if (event.target.value.match(/^[a-z A-Z À-ÿ -]{1,45}$/)) {
+    cityErrMsg.innerText = "";
+    cityInput = event.target.value;
+    console.log("Champs non vide", cityInput);
+  } else {
+    cityErrMsg.innerText = "Veuillez entrer un nom de ville valide";
+  }
+});
+
+mail.addEventListener("input", function (event) {
+  console.log(mail);
+  mailInput;
+  if (event.target.value.length == 0) {
+    mailInput = null;
+    mailErrMsg.innerText = "Veuillez entrer votre adresse mail";
+    console.log("Champs vide", mailInput);
+  } else if (event.target.value.length < 1 || event.target.value.length > 64) {
+    mailErrMsg.innerText = "Adresse mail invalide";
+    mailInput = null;
+  }
+  if (
+    event.target.value.match(
+      /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/
+    )
+  ) {
+    mailErrMsg.innerText = "";
+    mailInput = event.target.value;
+    console.log("Champs non vide", mailInput);
+  } else {
+    console.log("Veuillez entrer une adresse mail valide", mailInput);
+    mailErrMsg.innerText = "Adresse mail invalide";
+  }
+});
+
+SubmitBtn.addEventListener("click", function (e) {
+  // Gestion de la mise en LS
+  let contact = [];
+
+  // Création de l'objet contenant les informations à stocker dans le localStorage
+  console.log(localStorage.getItem("client"));
+  // Création de l'entrée client
+  let client = {
+    firstName: forenameInput,
+    lastName: surnameInput,
+    address: addressInput,
+    city: cityInput,
+    email: mailInput,
+  };
+
+  switch (client) {
+    case "firstName":
+    case "lastName":
+    case "address":
+    case "city":
+    case "email":
+      console.log(client);
+      contact.push(client);
+      console.log(contact);
+      break;
+
+    default:
+      e.preventDefault();
+      console.log("Erreur formulaire");
+      break;
+  }
+
+  // const Order = {
+  //   cart,
+  //   contact
+  // }
+
+  // if (forenameInput == 0) {
+  //   e.preventDefault();
+  //   console.log("Erreur formulaire");
+  // } else if (surnameInput == 0) {
+  //   console.log("Erreur formulaire");
+  // } else if (addressInput == 0) {
+  //   console.log("Erreur formulaire");
+  // } else {
+  //   contact.push(client);
+  // }
+
+  localStorage.setItem("client", JSON.stringify(contact));
+}); // Fin de l'eventlistener

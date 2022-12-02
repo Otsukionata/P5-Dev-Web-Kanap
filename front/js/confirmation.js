0b6e29c5d7757bb98455f7d8d72e4c60f74914bc
Otsukionata <e-ramassamy@laposte.net>
2022-12-02 14:34:42 +0100

    commentaires page confirmation

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,7 +1,9 @@
 "use strict";
 
+// ===================== Récupération du numéro de commande depuis l'URL de la page affichée
 const URL = new URLSearchParams(window.location.search);
 const ID = URL.get("id");
 
+// ===================== Affichage du numéro de commande dans le cadre dédié
 const orderId = document.querySelector("#orderId");
 orderId.innerText = ID;
